d951207ad2d1fa2aa9e87ae2309f790ef698ccd0
andermi <manders9@jhu.edu>
2018-03-28 16:36:25 -0400

    add script and setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='getreqt',
+    version='0.1',
+    description='get requirements tree for installed pip packages',
+    url='https://github.com/andermi/getreqt',
+    author='Michael Anderson',
+    author_email='manders9 at jhu dot edu',
+    license='MIT',
+    scripts=['getreqt'],
+    zip_safe=False
+)
